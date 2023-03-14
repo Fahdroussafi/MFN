b904487b9ba4d81716cfd40594278fad62b00f4c
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-03-14 10:56:19 +0100

    Create company.model.js

---
+++ api/src/company-service/company.model.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const companySchema = mongoose.Schema(
+  {
+    companyName: {
+      type: String,
+      required: [true, "Please add a company name"],
+    },
+    founder: {
+      type: String,
+      required: [true, "Please add a founder name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+    },
+    phone: {
+      type: Number,
+      required: [true, "Please add a phone number"],
+    },
+    address: {
+      type: String,
+      required: [true, "Please add an address"],
+    },
+    location: {
+      Latitude: {
+        type: Number,
+      },
+      Longitude: {
+        type: Number,
+      },
+      ICE: {
+        type: Number,
+        unique: true,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("company", companySchema);
