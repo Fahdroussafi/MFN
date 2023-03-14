2b1cb9e51e1bbf79f7e3be45fbff29a32ee9ce9f
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-03-14 11:06:08 +0100

    Create RootStack.js

---
+++ client/navigators/RootStack.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import LandingScreen from '../screens/LandingScreen';
+import MapScreen from '../screens/MapScreen';
+
+import Auth from './AuthStack';
+
+import {CredentialsContext} from '../components/CredentialsContext';
+
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return (
+    <CredentialsContext.Consumer>
+      {({storedCredentials}) => (
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{headerShown: false}}>
+            {storedCredentials ? (
+              <>
+                <Stack.Screen name="Map" component={MapScreen} />
+              </>
+            ) : (
+              <>
+                <Stack.Screen
+                  name="Landing"
+                  component={LandingScreen}
+                  options={{animation: 'slide_from_bottom'}}
+                />
+
+                <Stack.Screen
+                  name="Auth"
+                  component={Auth}
+                  options={{
+                    animation: 'slide_from_bottom',
+                  }}
+                />
+                {!storedCredentials && (
+                  <Stack.Screen
+                    name="Map"
+                    component={MapScreen}
+                    options={{animation: 'slide_from_right'}}
+                  />
+                )}
+              </>
+            )}
+          </Stack.Navigator>
+
+          <StatusBar backgroundColor="#000" barStyle="light-content" />
+        </NavigationContainer>
+      )}
+    </CredentialsContext.Consumer>
+  );
+}
+
+export default RootStack;
