75d90e8eae038c8611d8514206251a16eec79ba5
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-03-14 11:05:43 +0100

    images & colors needed

---
+++ client/constants/images.js
@@ -0,0 +1,17 @@
+export const eyeclosed = require('../assets/images/hidden.png');
+export const eye = require('../assets/images/view.png');
+export const lock = require('../assets/images/lock.png');
+export const person = require('../assets/images/person.png');
+export const background = require('../assets/images/backgroundagain.png');
+export const logo = require('../assets/images/logo.png');
+export const wallpaper = require('../assets/images/wallpaper.jpeg');
+
+export default {
+  logo,
+  eyeclosed,
+  eye,
+  person,
+  lock,
+  background,
+  wallpaper,
+};
